33d39dd4d3f7c8b7131fe0e33fa2ba34ceede695
Brocoruli <infernofirehell@gmail.com>
2023-11-27 20:29:25 +0100

    test: MakeADepositCorrectly

--- BankKata.Tests/BankControllerShould.cs
+++ BankKata.Tests/BankControllerShould.cs
@@ -1,4 +1,5 @@
 using System.Net;
+using System.Text.Json;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using WireMock.Server;
@@ -11,13 +12,24 @@ public class BankControllerShould
     public async void MakeADepositCorrectly()
     {
         using var client = GetTestHttpClient();
-        var server = WireMockServer.Start(7166);
-        var response = new HttpResponseMessage(HttpStatusCode.NotImplemented);
-
-        response = await client.PostAsync("Bank/Deposit/amount",
+        var responseDeposit = new HttpResponseMessage(HttpStatusCode.NotImplemented);
+ 
+        responseDeposit = await client.PostAsync("Bank/Deposit/amount",
             new StringContent("500"));
 
-        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        Assert.Equal(HttpStatusCode.OK, responseDeposit.StatusCode);
+
+        var response = new HttpResponseMessage(HttpStatusCode.NotImplemented);
+
+        response = await client.GetAsync("Bank/GetStatement");
+        var jsonResponse = await response.Content.ReadAsStringAsync();
+        var result = JsonSerializer.Deserialize<Account>(jsonResponse);
+        var expectedStatement = new Account()
+        {
+            Balance = 500,
+            Movements = { new Movement(500, 500) }
+        };
+        Assert.Equal(expectedStatement, result);
     }
 
     private HttpClient GetTestHttpClient()
@@ -32,4 +44,20 @@ public class BankControllerShould
         });
         return client;
     }
+}
+
+public class Account
+{
+    public int Balance { get; set; }
+    public readonly List<Movement> Movements = new();
+}
+
+public class Movement
+{
+    public Movement(int amount, int balance)
+    {
+        Statement = $" || {amount} || {balance}";
+    }
+
+    public string Statement { get; set; }
 }
