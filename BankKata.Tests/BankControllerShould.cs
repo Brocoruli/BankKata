120525a0d883bea5094aa387eb1321c2cec45c27
Brocoruli <infernofirehell@gmail.com>
2023-12-07 12:05:19 +0100

    test: MakeAWithdrawCorrectly

--- BankKata.Tests/BankControllerShould.cs
+++ BankKata.Tests/BankControllerShould.cs
@@ -40,6 +40,39 @@ public class BankControllerShould
         };
         Assert.Equal(expectedAccount.Balance, result);
     }
+    
+    [Fact]
+    public async void MakeAWithdrawCorrectly()
+    {
+        using var client = GetTestHttpClient();
+        var responseWithdraw = new HttpResponseMessage(HttpStatusCode.NotImplemented);
+        var accountRequest = new AccountRequest()
+        {
+            id = 2,
+            amount = -500
+        };
+        var request = JsonSerializer.Serialize(accountRequest);
+        
+        responseWithdraw = await client.PostAsync("Bank/Withdraw/amount",
+            new StringContent(request, Encoding.UTF8, "application/json"));
+
+        Assert.Equal(HttpStatusCode.OK, responseWithdraw.StatusCode);
+
+        
+        var response = new HttpResponseMessage(HttpStatusCode.NotImplemented);
+        var id = 2;
+        
+        response = await client.GetAsync($"Bank/GetStatement/{id}");
+        
+        var jsonResponse = await response.Content.ReadAsStringAsync();
+        var result = JsonSerializer.Deserialize<int>(jsonResponse);
+        var expectedAccount = new Account()
+        {
+            Balance = 0,
+            Movements = { new Movement(500, 500), new Movement(-500, 0) }
+        };
+        Assert.Equal(expectedAccount.Balance, result);
+    }
 
     private HttpClient GetTestHttpClient()
     {
