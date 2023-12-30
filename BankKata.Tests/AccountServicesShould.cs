152e2933fb5151750df1a08bfc250bab2da4c7ac
Brocoruli <infernofirehell@gmail.com>
2023-12-30 18:26:08 +0100

    test: DepositAmountCorrectlyInTheAccount

---
+++ BankKata.Tests/AccountServicesShould.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+
+namespace BankKata.Tests;
+
+public class AccountServicesShould
+{
+    private readonly AccountServices _accountServices;
+    private readonly AccountRepository _accountRepository = new();
+
+    public AccountServicesShould()
+    {
+        _accountServices = new AccountServices(_accountRepository);
+    }
+
+    [Fact]
+    public void DepositAmountCorrectlyInTheAccount()
+    {
+        var accountRequest = new AccountRequest()
+        {
+            Id = 1,
+            Amount = 500
+        };
+        
+        _accountServices.Deposit(accountRequest);
+
+        var balance = _accountRepository.GetBalance(1);
+        const int expectedBalance = 500;
+        balance.Should().Be(expectedBalance);
+    }
+}
---
+++ BankKata/AccountServices.cs
@@ -0,0 +1,16 @@
+﻿namespace BankKata;
+
+public class AccountServices
+{
+    private AccountRepository _accountRepository;
+    
+    public AccountServices(AccountRepository accountRepository)
+    {
+        _accountRepository = accountRepository;
+    }
+
+    public void Deposit(AccountRequest accountRequest)
+    {
+        throw new NotImplementedException();
+    }
+}
