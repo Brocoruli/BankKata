3c42de2abb0708b8daa377e4bc783a9b08fd8f8b
Brocoruli <infernofirehell@gmail.com>
2024-02-08 19:59:39 +0100

    test: Add WithdrawAmountCorrectlyInTheAccount test

--- BankKata.Tests/Integration/AccountServicesShould.cs
+++ BankKata.Tests/Integration/AccountServicesShould.cs
@@ -21,7 +21,7 @@ public class AccountServicesShould
     public async Task DepositAmountCorrectlyInTheAccount()
     {
         _testContext.BankKataDbContext.Accounts.AddAsync(new Account(new Guid("881b2297-1c8f-4ef2-b80c-bfa5a43107ae"), 0));
-        _testContext.BankKataDbContext.Movements.AddAsync(new Movement(Guid.NewGuid(),new Guid("881b2297-1c8f-4ef2-b80c-bfa5a43107ae"), 0, 500));
+        _testContext.BankKataDbContext.Movements.AddAsync(new Movement(Guid.NewGuid(),new Guid("881b2297-1c8f-4ef2-b80c-bfa5a43107ae"), 0, 0));
         _testContext.BankKataDbContext.SaveChangesAsync();
         
         var accountId = new Guid("881b2297-1c8f-4ef2-b80c-bfa5a43107ae");
@@ -35,6 +35,27 @@ public class AccountServicesShould
         
         var balance = await _accountRepository.GetBalance(accountId);
         const int expectedBalance = 500;
+        balance.Should().Be(expectedBalance);
+    }
+    
+    [Fact]
+    public async Task WithdrawAmountCorrectlyInTheAccount()
+    {
+        _testContext.BankKataDbContext.Accounts.AddAsync(new Account(new Guid("2d61906c-d856-4b3b-89b1-67673ee5499c"), 500));
+        _testContext.BankKataDbContext.Movements.AddAsync(new Movement(Guid.NewGuid(),new Guid("2d61906c-d856-4b3b-89b1-67673ee5499c"), 500, 500));
+        _testContext.BankKataDbContext.SaveChangesAsync();
+        
+        var accountId = new Guid("2d61906c-d856-4b3b-89b1-67673ee5499c");
+        var accountRequest = new AccountRequest()
+        {
+            Id = accountId,
+            Amount = 500
+        };
+        
+        await _accountServices.Withdraw(accountRequest);
+        
+        var balance = await _accountRepository.GetBalance(accountId);
+        const int expectedBalance = 0;
         balance.Should().Be(expectedBalance);
     }
 }
